b703f3f59196ced66ca7f1beb2e6f5561bc08124
Levin Schmid <levin.schmid@mein.gmx>
2024-12-04 15:48:20 +0100

    Add project files.

---
+++ OOP/Kontakt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Kontakt
+    {
+        string firstName;
+        string lastName;
+        string email;
+
+        public string FirstName 
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kontakt> kontakte = new List<Kontakt>();
+            while (true)
+            {
+
+
+                Console.WriteLine("Wollen sie einen neuen Kontakt anlegen? (j/n)");
+                string input = Console.ReadLine();
+
+                if (input == "j")
+                {
+                    Kontakt k = new Kontakt();
+                    Console.Write("Vorname:");
+                    string firstName = Console.ReadLine();
+                    Console.Write("Nachname:");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Email:");
+                    string email = Console.ReadLine();
+
+                    k.FirstName = firstName;
+                    k.LastName = lastName;
+                    k.Email = email;
+
+                    kontakte.Add(k);
+                }
+                else
+                {
+                    Console.WriteLine("Willst du zuerst mit Vor- oder Nachnamen ausgeben? [v,n]");
+                    string input2 = Console.ReadLine();
+                    if (input2 == "v")
+                    {
+                        foreach (Kontakt k in kontakte)
+                        {
+                            Console.WriteLine(k.FirstName + "," + k.LastName + "(" + k.Email + ")");
+                        }
+                    }
+                    else
+                    {
+                        foreach (Kontakt k in kontakte)
+                        {
+                            Console.WriteLine(k.LastName + "," + k.FirstName + "(" + k.Email + ")");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
